06f31540583287fb208c3b183cfb3e9d83a9e078
lulu <lulu@luludeMacBook-Pro.local>
2019-09-24 07:24:38 +0800

    add action

---
+++ app/src/main/java/com/example/myapplication/advancestrangely/ActionUser.java
@@ -0,0 +1,98 @@
+package com.example.myapplication.advancestrangely;
+
+import android.media.audiofx.DynamicsProcessing;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+public class ActionUser implements ApplicationListener, InputProcessor {
+
+    Texture starTexture;
+    Texture ballTexture;
+
+    Stage stage;
+    Image image;
+    float delta;
+
+    @Override
+    public void create() {
+        Gdx.input.setInputProcessor(this);
+    }
+
+    public void createStar(boolean star, int x, int y, int size){
+        if(star == true){
+            image = new Image(starTexture);
+        }else {
+
+        }
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public boolean keyDown(int i) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int i) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char c) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int i, int i1, int i2) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int i) {
+        return false;
+    }
+}
