8c1032aabe039a0749a51e65de83b0cbae953c04
lcmftianci <707010543@qq.com>
2019-11-14 12:44:15 +0800

    update threeseconds background

--- app/src/main/java/com/lcmf/mmgo/threelife/FastDie.java
+++ app/src/main/java/com/lcmf/mmgo/threelife/FastDie.java
@@ -1,12 +1,33 @@
 package com.lcmf.mmgo.threelife;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
+import javax.microedition.khronos.opengles.GL10;
 
 public class FastDie implements ApplicationListener {
 
+    Sprite sprite;
+    SpriteBatch spriteBatch;
+    Texture background;
+    Texture airPlane;
+    TextureAtlas atlas;
+
     @Override
     public void create() {
+        spriteBatch = new SpriteBatch();
+        background = new Texture(Gdx.files.internal("mubu/mubu.jpg"));
+        airPlane = new Texture(Gdx.files.internal("shoot/enemy1.png"));
+        //atlas = new TextureAtlas(Gdx.files.internal(""));
+        //sprite = atlas.createSprite("");
+        //sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
+        sprite = new Sprite(airPlane, 0,0,49,37);
+        sprite.setSize(Gdx.graphics.getWidth()/15*4/3, Gdx.graphics.getWidth()/15);
     }
 
     @Override
@@ -16,7 +37,13 @@ public class FastDie implements ApplicationListener {
 
     @Override
     public void render() {
-
+        Gdx.gl.glClearColor(1,1,1,1);
+        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+        spriteBatch.begin();
+        spriteBatch.draw(background,0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        sprite.setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+        sprite.draw(spriteBatch);
+        spriteBatch.end();
     }
 
     @Override
